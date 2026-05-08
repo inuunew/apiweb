6a03219ef8ef57bc2799090763ceb2b198aab34b
inuunew <choirulazzamibnu@gmail.com>
2026-05-08 17:02:22 +0700

    Create reminder.js

---
+++ api/reminder.js
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.status(200).json({
+        "message": "SYSTEM ONLINE • WAITING FOR CONFIG..."
+    });
+}
