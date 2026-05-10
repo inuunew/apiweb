f1d7539590e9ff335d3b89889f230a0b1874ae8e
inuunew <choirulazzamibnu@gmail.com>
2026-05-10 23:47:43 +0700

    Update [type].js

--- api/tools/[type].js
+++ api/tools/[type].js
@@ -78,7 +78,7 @@ export default async function handler(req, res) {
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Secured by NuuTools API</title>
+<title>Encrypt HTML</title>
 </head>
 <body>
 <script>
