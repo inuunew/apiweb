ee6b9f1a73574ae70f5e54ebcca9f12278c71515
inuunew <choirulazzamibnu@gmail.com>
2026-05-12 23:12:22 +0700

    Delete reminder.js

--- api/reminder.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.status(200).json({
-        "message": "SYSTEM ONLINE • WAITING FOR CONFIG..."
-    });
-}
